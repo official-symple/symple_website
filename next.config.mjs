a8e90773c5a7befa49b7037811d41e1c888781a0
koreamango <mango1229@icloud.com>
2025-12-20 15:39:10 +0900

    feat: add aws build script

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // Enable static HTML export for S3 hosting
+  output: "export",
+  // Use trailing slashes to improve S3 website compatibility
+  trailingSlash: true,
   eslint: {
     ignoreDuringBuilds: true,
   },
@@ -9,6 +13,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
